a53e9231d8cb2672b5ba2bf43b400fb3537b0edb
VivianeMarina <202501673163@alunos.estacio.br>
2025-06-03 15:04:19 -0300

    Update batalhaNaval_NívelNovato.c
    Exercício Batalha Naval nível novato, tema 4

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,6 +10,46 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
+    // Fazer o posicionamento dos navios
+
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; 
+    int tabuleiro [10][10] = { // Vai de 0 a 9 nas duas dimensões (totalizando 10)
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 3, 3, 3, 0, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    
+    
+    for (int letra = 0; letra < 10; letra++) {
+        printf ("\t%c", linha[letra]); // impressão das colunas 
+    }
+    
+    printf ("\n");
+
+    int l = 1;
+
+    for (int x = 0; x < 10; x++) {
+        printf ("%d", l);
+        l++;
+        for (int y = 0; y < 10; y++){
+            printf ("\t%d", tabuleiro[x][y]);
+        }                
+        printf ("\n"); 
+    }
+
+
+}
+
+
+
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
